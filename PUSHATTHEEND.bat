352852db8bf7530aa791abd09eff2450589466a6
Ikkinai <sumtrec@ya.ru>
2018-09-22 14:27:04 +0300

    system-config-date

---
+++ PUSHATTHEEND.bat
@@ -0,0 +1,10 @@
+git add *.filters
+git add *.h
+git add *.cpp 
+git add *.sln 
+git add *.vcxproj 
+git add *.user
+git add *.bat
+git commit -am system-config-date
+git push iamiki master
+pause
---
+++ autocommitushka.bat
@@ -0,0 +1,3 @@
+git add *.h *.cpp *.sln *.vcxproj *.filters
+git commit
+pause
